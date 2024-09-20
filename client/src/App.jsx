7e13fb403bf42b72be316ece9671ab1f2d963bc8
rastar12 <eugenechanzu@gmail.com>
2024-09-20 11:16:39 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/navBar'
+import Hero from './components/Hero'
+
+export default function App() {
+  return (
+    <main className='overflow-x-hidden'>
+      <NavBar/>
+      <Hero/>
+      </main>
+  )
+}
